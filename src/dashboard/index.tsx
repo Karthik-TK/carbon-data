cf8e42fbc09241c39e3c3f41ff047690efc1a5e7
Karthik TK <karthiktk16@gmail.com>
2023-08-27 20:43:01 +0530

    display dashboard ui

---
+++ src/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./navbar";
+import { Feature } from "./feature";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <NavBar />
+      <Feature />
+    </div>
+  );
+};
+
+export default Dashboard;
