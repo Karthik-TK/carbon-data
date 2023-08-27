99908c12d78fd123a22c6f83e827ae893124db13
Karthik TK <karthiktk16@gmail.com>
2023-08-27 20:42:10 +0530

    handle react router for components

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ComingSoon from "./pages/404";
+import FormUI from "./form/form-ui";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ComingSoon />,
+  },
+  {
+    path: "/signup",
+    element: <ComingSoon />,
+    errorElement: <ComingSoon />,
+  },
+  {
+    path: "form",
+    element: <FormUI />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
