20622ce4b909aba4ee2804b190339bb6e8454762
Karthik TK <karthiktk16@gmail.com>
2023-08-28 13:16:27 +0530

    fix svg syntax

--- src/pages/loadScreen.tsx
+++ src/pages/loadScreen.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadScreen = () => {
   return (
-    <div className="absolute flex justify-center items-center w-full h-full top-0 left-0 backdrop-filter backdrop-blur-sm bg-gray-100 bg-opacity-5 transition-color z-60 cursor-progress">
+    <div className="absolute flex justify-center items-center w-full h-screen top-0 left-0 backdrop-filter backdrop-blur-sm bg-gray-100 bg-opacity-5 transition-color z-60 cursor-progress">
       <div className="block text-center">
         <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
           <svg
@@ -12,10 +12,10 @@ const LoadScreen = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              clip-rule="evenodd"
+              clipRule="evenodd"
               d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
               fill="currentColor"
-              fill-rule="evenodd"
+              fillRule="evenodd"
             />
           </svg>
 
